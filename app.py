9fafbb456bfe6f1a948dff1de46bfe72260ead7f
Axel <axelmierczuk@gmail.com>
2022-01-02 15:07:29 -0500

    Added commands functionality and struct.

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, request
+from structs import commands
+import json
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return ""
+commands = commands.Commands()
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '<h1>Sanity Check</h1>'
+
+
+@app.route('/api/v1/commands', methods=['POST', 'GET'])
+def request_commands():
+    # content = request.json
+    data = dict()
+    if request.method == 'GET':
+        data['commands'] = commands.get_commands_unique()
+    else:
+        data['commands'] = commands.push_commands(request.json)
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
---
+++ structs/commands.py
@@ -0,0 +1,27 @@
+import datetime
+
+
+class Commands:
+    def __init__(self):
+        self.commands = []
+
+    def push_commands(self, req_content: dict) -> list:
+        for _id, command in req_content.items():
+            self.commands.append(command)
+        return self.commands
+
+    def push_commands_unique(self, req_content: dict) -> list:
+        for _id, command in req_content.items():
+            if command not in self.commands:
+                self.commands.append(command)
+        return self.commands
+
+    def get_commands(self) -> list:
+        return self.commands
+
+    def get_commands_unique(self) -> list:
+        res = []
+        for c in self.commands:
+            if c not in res:
+                res.append(c)
+        return res
