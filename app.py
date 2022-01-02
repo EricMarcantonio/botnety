66c6e09758a2964dcacb21cc2d99e6b1110b5af3
Axel <axelmierczuk@gmail.com>
2022-01-02 18:17:23 -0500

    Added error checking.

--- app.py
+++ app.py
@@ -15,10 +15,20 @@ def home():
 def request_commands():
     # content = request.json
     data = dict()
-    if request.method == 'GET':
-        data['commands'] = commands.get_commands_unique()
-    else:
+    if request.method == 'POST':
+        if 'commands' not in request.json:
+            return "Commands list is not present.", 400
+        if len(request.json) != 1:
+            return "Request is not the right length.", 400
+        if not isinstance(request.json['commands'], list):
+            return "Commands is not a list.", 400
+        if len(request.json['commands']) == 0:
+            return "Commands array does not contain commands.", 400
+        if not all(isinstance(x, str) for x in request.json['commands']):
+            return "Commands array should only contain strings.", 400
         data['commands'] = commands.set_commands(request.json)
+    else:
+        data['commands'] = commands.get_commands_unique()
     response = app.response_class(
         response=json.dumps(data),
         status=200,
